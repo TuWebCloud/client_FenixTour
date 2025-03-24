9f2e4a9fc022e28613ae52741bc662204193d2ca
Diego <diegovalrus@gmail.com>
2025-03-24 12:45:56 -0400

    mod: pc-resp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,35 @@
+import { useEffect, useState } from 'react'
 import ServiceQuest from './ServiceQuest'
 import './styles/Hero.css'
 
 export default function Hero ({ transporte }) {
+    const [vehiculo, setVehiculo] = useState('./public/furgoneta-hero.png')
+    
+    useEffect(() => {
+        if (window.innerWidth >= 600) {
+            setVehiculo('/public/microbus-pc.png')
+        }
+    }, [])
+
     return (
         <section className='section-hero'>
-            <h2 className='subtitulo-hero'>Viaja con <br/><span>nosotros</span></h2>
-            <img src={transporte} alt="Microbus" className='microbus-img' />
+            <h2 className='subtitulo-hero'>Viaja con <br/>nosotros</h2>
+            <img src={vehiculo} alt="Microbus" className='microbus-img' />
             <img src='../../public/cuba.svg' alt="Mapa de Cuba" className='cuba-img' />
 
-            <div className="layer-1"></div>
-            <div className="layer-2"></div>
-            <div className="layer-3"></div>
-            <div className="layer-4">
-                <div className="color-1 color"></div>
-                <div className="color-2 color"></div>
-                <div className="color-3 color"></div>
-                <div className="color-4 color"></div>
-            </div>
+            {window.innerWidth < 600 && // todo
+                <>
+                    <div className="layer-1"></div>
+                    <div className="layer-2"></div>
+                    <div className="layer-3"></div>
+                    <div className="layer-4">
+                        <div className="color-1 color"></div>
+                        <div className="color-2 color"></div>
+                        <div className="color-3 color"></div>
+                        <div className="color-4 color"></div>
+                    </div>
+                </>
+            }
 
             <section>
                 <div>
